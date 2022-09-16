4bd8615320cd1f48c641aca730bc776b69cb8d53
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-09-16 13:50:01 -0600

    added api calls for reporting and marking helpful

--- PostgresDB/DBLogic.js
+++ PostgresDB/DBLogic.js
@@ -1,21 +1,17 @@
 const { Pool, Client } = require('pg')
 const pool = new Pool();
-const client = new Client();
 
-var poolOpen = false;
-
-const getQuestions = async (prodId, count = 5) => {
-
-  if (!poolOpen) {
-    await pool.connect(err => {
-      if (err) {
-        console.error('connection error', err.stack)
-      } else {
-        console.log('connected')
-        poolOpen = true;
-      }
-    })
+pool.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('Pool open')
+    poolOpen = true;
   }
+})
+
+// <--------------- QUESTION LOGIC ----------------->
+const getQuestions = async (prodId, count = 5) => {
 
   var result = await pool.query('SELECT question_id, question_body, question_date, asker_name, question_helpfulness FROM questions WHERE product_id = $1 AND reported = 0 LIMIT $2', [prodId, count])
 
@@ -40,45 +36,36 @@ const getQuestions = async (prodId, count = 5) => {
   return {product_id: prodId, results: resultPromise};
 }
 
+const addQuestion = async ({ body, name, email, product_id }) => {
+  await pool.query(`INSERT INTO questions (product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness) VALUES ( $1, $2, $3, $4, $5, 0, 0);`, [product_id, body, new Date().getTime(), name, email])
+}
+
+const helpfulQuestion = async (question_id) => {
+  return await pool.query(`UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = $1;`, [question_id])
+}
+
+const reportQuestion = async (question_id) => {
+  return await pool.query(`UPDATE questions SET reported = 1 WHERE question_id = $1;`, [question_id])
+}
+// <--------------- ANSWERS LOGIC ----------------->
+
 const getAnswers = async (question_id, count = 5, page = 0) => {
 
-  if (!poolOpen) {
-    await pool.connect(err => {
-      if (err) {
-        console.error('connection error', err.stack)
-      } else {
-        console.log('connected')
-        poolOpen = true;
-      }
-    })
-  }
-
   var results = await pool.query(`SELECT answer_id, body, date, answerer_name, helpfulness FROM answers WHERE question_id = $1 AND reported = 0 LIMIT $2`, [question_id, count]).then(async data => {
-    console.log(data.rows)
     for (var i = 0; i < data.rows.length; i++) {
       await pool.query(`SELECT id, url FROM pictures WHERE answer_id = $1`, [data.rows[i].answer_id]).then(photosData => {
-        console.log('photo: ', photosData.rows)
         data.rows[i].photos = photosData.rows;
       })
     }
     return data.rows
   })
-  console.log(results)
+  for (var i = 0; i < results.length; i++) {
+    results[i].date = new Date(parseInt(results[i].date)).toLocaleDateString('sv').replace(/\//g, '-');
+  }
   return {question: question_id, page: page, count: count, results: results};
 }
 
 const addAnswer = async ( { question_id }, {body, name, email, photos}) => {
-
-  if (!poolOpen) {
-    await pool.connect(err => {
-      if (err) {
-        console.error('connection error', err.stack)
-      } else {
-        console.log('connected')
-        poolOpen = true;
-      }
-    })
-  }
 
   var lastId = await pool.query(`INSERT INTO answers (question_id, body, date, answerer_name, answerer_email, reported, helpfulness) VALUES ( $1, $2, $3, $4, $5, 0, 0) RETURNING answer_id;`, [question_id, body, new Date().getTime(), name, email])
   lastId = lastId.rows[0].answer_id;
@@ -88,4 +75,12 @@ const addAnswer = async ( { question_id }, {body, name, email, photos}) => {
   })
 }
 
-module.exports = { getQuestions, getAnswers, addAnswer }
+const helpfulAnswer = async (answer_id) => {
+  return await pool.query(`UPDATE answers SET helpfulness = helpfulness + 1 WHERE answer_id = $1;`, [answer_id])
+}
+
+const reportAnswer = async (answer_id) => {
+  return await pool.query(`UPDATE answers SET reported = 1 WHERE answer_id = $1;`, [answer_id])
+}
+
+module.exports = { getQuestions, addQuestion, helpfulQuestion, reportQuestion, getAnswers, addAnswer, helpfulAnswer, reportAnswer}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const { getQuestions, getAnswers, addAnswer } = require('../PostgresDB/DBLogic.js')
+const { getQuestions, addQuestion, helpfulQuestion, reportQuestion, getAnswers, addAnswer, helpfulAnswer, reportAnswer } = require('../PostgresDB/DBLogic.js')
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+//get questions
 app.get('/qa/questions', (req, res) => {
   getQuestions(req.query.product_id, req.query.count).then(data => {
     res.status(200);
@@ -18,6 +15,37 @@ app.get('/qa/questions', (req, res) => {
   })
 })
 
+//add question
+app.post('/qa/questions', (req, res) => {
+  addQuestion(req.body).then(() => {
+    res.sendStatus(201);
+  }).catch(err => {
+    console.log('POST error: ', err);
+    res.sendStatus(404);
+  })
+})
+
+//mark question as helpful
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  helpfulQuestion(req.params.question_id).then(() => {
+    res.sendStatus(204);
+  }).catch(err => {
+    console.log('PUT error: ', err);
+    res.sendStatus(404);
+  })
+})
+
+//report question
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  reportQuestion(req.params.question_id).then(() => {
+    res.sendStatus(204);
+  }).catch(err => {
+    console.log('PUT error: ', err);
+    res.sendStatus(404);
+  })
+})
+
+//get answers
 app.get('/qa/questions/:question_id/answers', (req, res) => {
   getAnswers(req.params.question_id, req.query.count).then(data => {
     res.status(200);
@@ -28,6 +56,7 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
   })
 })
 
+//add answer
 app.post('/qa/questions/:question_id/answers', (req, res) => {
   addAnswer(req.params, req.body).then(() => {
     res.sendStatus(201);
@@ -37,6 +66,25 @@ app.post('/qa/questions/:question_id/answers', (req, res) => {
   })
 })
 
+//mark answer as helpful
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  helpfulAnswer(req.params.answer_id).then(() => {
+    res.sendStatus(204);
+  }).catch(err => {
+    console.log('PUT error: ', err);
+    res.sendStatus(404);
+  })
+})
+
+//report answer
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  reportAnswer(req.params.answer_id).then(() => {
+    res.sendStatus(204);
+  }).catch(err => {
+    console.log('PUT error: ', err);
+    res.sendStatus(404);
+  })
+})
 
 const port = 3000;
 
